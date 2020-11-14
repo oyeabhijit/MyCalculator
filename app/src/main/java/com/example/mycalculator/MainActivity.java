c19ce18b8dec6ea7516d23dbc8a265968f785af8
Abhijit Rout <abhijitroutabcd@gmail.com>
2020-11-14 10:32:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mycalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,bdot,bpi,bequal,bplus,bmin,bsum,bdiv,bsqrt,bsquare
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
